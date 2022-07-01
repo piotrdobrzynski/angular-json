887dfd9c9051aebb49b4e2b399db97e187463e9a
Piotr Dobrzyński <50482622+piotrdobrzynski@users.noreply.github.com>
2022-07-01 14:43:57 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PokemonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
